1da15a61e7fe1148a43f15fe8f01da623932f73c
natsukoitou <n.itou@ship-inc.jp>
2025-04-19 21:35:12 +0900

    fix: マイグレーションファイルの修正
    - PHPオープニングタグを修正しました。
    - 'origin_country_id'のコメントのスペルミスを修正しました。

--- dog-breed-app-api/src/database/migrations/2025_04_19_114755_create_breeds_table.php
+++ dog-breed-app-api/src/database/migrations/2025_04_19_114755_create_breeds_table.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -13,7 +13,7 @@ return new class extends Migration
             $table->string('name_ja', 100)->nullable()->comment('犬種名（日本語）');
             $table->string('name_en', 100)->nullable()->comment('犬種名（英語）');
             $table->tinyInteger('size_class')->nullable()->comment('サイズ');
-            $table->foreignId('origin_country_id')->constrained('countries')->commert('原産国ID');
+            $table->foreignId('origin_country_id')->constrained('countries')->comment('原産国ID');
             $table->text('description')->nullable()->comment('犬種説明');
             $table->string('image_path', 255)->nullable()->comment('画像ファイルパス');
             $table->timestamps();
